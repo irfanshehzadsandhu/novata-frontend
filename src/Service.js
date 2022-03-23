3cc8e131481440c01e79e6dd6bd20bbd8da54bdf
irfanshehzadsandhu <irfanshehzadsandhu@gmail.com>
2022-03-24 00:42:38 +0500

    refactor route name

--- src/Service.js
+++ src/Service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {serverUrl} from "./Constants";
 
 const getFormDetails = async () => {
-  return await axios.get(`${serverUrl}/api/v1/form/fetchOne`);
+  return await axios.get(`${serverUrl}/api/v1/forms/00b98f36-8d0b-44a1-9d12-2d05fbe0f3ee`);
 }
 
 const saveFormDetails = async (data) => {
